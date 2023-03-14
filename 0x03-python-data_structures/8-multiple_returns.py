4599cd0129b796bbde9c0850177c86fc867dd968
GIT-justin12 <agbozojustin42@gmail.com>
2023-03-14 07:50:49 -0700

    Task 8: Update 2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else "None"
-    tup = lenght, first_char
+    tup = length, first_char
     return(tup)
