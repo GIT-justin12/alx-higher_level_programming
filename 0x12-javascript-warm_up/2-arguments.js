e52f59b27e9ab63d3295d0b930b63d70aeffab4d
GIT-justin12 <agbozojustin42@gmail.com>
2023-07-01 10:44:18 -0700

    Update 3 of task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 !#/usr/bin/node
 
-const lengthOfArgs = process.argv.length;
 
-if (lengthOfArgs === 2){
+if ( process.argv.length === 2){
 	console.log('No argument');
-}else if(lengthOfArgs === 3){
+}else if( process.argv.length === 3){
 	console.log('Argument found');
 }else{
 	console.log('Arguments found');
