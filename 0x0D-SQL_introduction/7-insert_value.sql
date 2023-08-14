c7592b00e7a752cb0b23be07df3a724dae2e12cc
GIT-justin12 <agbojustin42@gmail.com>
2023-08-14 09:00:35 -0700

    Update 1 of task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new in the table `first_table`
 -- in database `hbtn_0c_0` in MySQL Server.
-INSERT INTO `first_table` (`ìd`, `name`) VALUES (89, "Holberton School")
+INSERT INTO `first_table` (`ìd`, `name`) VALUES (89, "Holberton School");
