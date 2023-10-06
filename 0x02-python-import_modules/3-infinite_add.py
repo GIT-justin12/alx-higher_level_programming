94d056a4fef274190e3af1859d35bab31ad3cd12
GIT-justin12 <agbojustin42@gmail.com>
2023-10-06 09:21:22 -0700

    Update 1 of task 1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,12 +2,12 @@
 import sys
 
 if __name__ == '__main__':
-    arg_array = sys.argv
-    arg_array_len = len(arg_array)
+    arg_arr = sys.argv
+    arg_arr_len = len(arg_arr)
     sum = 0
 
     if arg_array_len > 1:
-        for i in range(1, arg_array_len):
-            sum += int(arg_array[i])
+        for i in range(1, arg_arr_len):
+            sum += int(arg_arr[i])
 
     print(sum)
